6949120351e0e356f2fda9566c99b07aa475ed00
moises-dias <moisesdepaulodias@gmail.com>
2023-09-26 19:41:12 -0300

    refactoring bounding box generator

---
+++ collision_4_classes.py
@@ -0,0 +1,223 @@
+import pygame
+import sys
+import math
+
+pygame.init()
+
+SCREEN_WIDTH = 1200
+SCREEN_HEIGHT = 800
+
+squares_1 = [
+[(530, 180), (530, 380), (580, 380), (580, 180)],
+[(390, 180), (390, 380), (430, 380), (430, 180)],
+[(430, 330), (430, 380), (530, 380), (530, 330)],
+[(430, 180), (430, 230), (530, 230), (530, 180)],
+[(460, 110), (460, 180), (520, 180), (520, 110)],
+[(350, 30), (350, 110), (630, 110), (630, 30)],
+[(350, -170), (350, 30), (460, 30), (460, -170)],
+[(530, -170), (530, 30), (630, 30), (630, -170)],
+[(460, -170), (460, -80), (530, -80), (530, -170)]
+]
+
+
+isometrics = [
+[(350, 355), (150, 455), (200, 480), (400, 380)],
+[(210, 285), (10, 385), (50, 405), (250, 305)],
+[(100, 380), (50, 405), (150, 455), (200, 430)],
+[(250, 305), (200, 330), (300, 380), (350, 355)],
+[(350, 285), (280, 320), (340, 350), (410, 315)],
+[(320, 190), (240, 230), (520, 370), (600, 330)],
+[(520, 90), (320, 190), (430, 245), (630, 145)],
+[(700, 180), (500, 280), (600, 330), (800, 230)],
+[(630, 145), (540, 190), (610, 225), (700, 180)]
+]
+
+
+border = 15
+square_point = (560, 290)
+walker_x = square_point[0]
+walker_y = square_point[1]
+
+isometric_point = (270, 425)
+isometric_walker_x = isometric_point[0]
+isometric_walker_y = isometric_point[1]
+
+
+# gerando o isometric do personagem na tela
+# [(530, 180), (530, 380), (580, 380), (580, 180)] up-left, down-left, down-right, up-right
+square_reference = [(-border, -border), (-border, border), (border, border), (border, -border)]
+
+angle = math.radians(45)
+sin_angle = math.sin(angle)
+cos_angle = math.cos(angle)
+sqrt_2 = math.sqrt(2)
+
+isometric_reference = []
+for x, y in square_reference:
+    new_x = 2*(x * cos_angle - y * sin_angle)/sqrt_2
+    new_y = (x * sin_angle + y * cos_angle)/sqrt_2
+    isometric_reference.append((round(new_x), round(new_y)))
+
+print(isometric_reference)
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+clock = pygame.time.Clock()
+font = pygame.font.Font(None, 36)
+image = pygame.image.load("assets/isometric.jpg")
+image_rect = image.get_rect()
+
+velocity = 2
+velocity_reducer = 0.5
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    new_walker_x = 0
+    new_walker_y = 0
+    isometric_new_walker_x = 0
+    isometric_new_walker_y = 0
+
+    left = 0
+    right = 0
+    up = 0
+    down = 0
+
+    keys = pygame.key.get_pressed()
+
+
+    if keys[pygame.K_LEFT]:
+        left = 1
+    if keys[pygame.K_RIGHT]:
+        right = 1
+    if keys[pygame.K_UP]:
+        up = 1
+    if keys[pygame.K_DOWN]:
+        down = 1
+
+
+    # se o resultado ta dentro de algum squares_1, seta os moves e vai pro proximo
+    updated_1 = False
+    
+    new_walker_x = (right - left) * velocity + (down - up) * velocity
+    new_walker_y = (left - right) * velocity + (down - up) * velocity
+    isometric_new_walker_x = 2 * (right - left) * velocity
+    isometric_new_walker_y = (down - up) * velocity
+
+    if left == 1 or right == 1:
+        new_walker_x *= velocity_reducer
+        new_walker_y *= velocity_reducer
+        isometric_new_walker_x *= velocity_reducer
+        isometric_new_walker_y *= velocity_reducer
+
+    for sq1 in squares_1:
+        x1 = sq1[0][0]
+        y1 = sq1[0][1]
+        x2 = sq1[2][0]
+        y2 = sq1[2][1]
+
+
+        if  x1 <= walker_x + new_walker_x <= x2 and y1 <= walker_y + new_walker_y <= y2:
+
+            walker_x += new_walker_x
+            walker_y += new_walker_y
+            isometric_walker_x += isometric_new_walker_x
+            isometric_walker_y += isometric_new_walker_y
+
+            updated_1 = True
+            break
+
+    # se nao atualizou é pq tentou sair dos limites
+    # preciso checar se so uma direcao foi pressionada???????????
+    if not updated_1 and (up + down + left + right) == 1:
+        # se chegou ate aqui é uma ou as duas direcoes nao fica dentro de um quadrado andavel
+        # se x nao esta em nenhum quadrado, e y nao esta em nenhum quadrado, nao anda
+        # senao
+        # se x em algum quadrado, anda em x
+        # se y em algum quadrado, anda em y
+
+        for sq1 in squares_1:
+            x1 = sq1[0][0]
+            y1 = sq1[0][1]
+            x2 = sq1[2][0]
+            y2 = sq1[2][1]
+
+            if  not (x1 <= walker_x <= x2 and y1 <= walker_y <= y2):
+                continue
+
+            if  x1 <= walker_x + new_walker_x <= x2 or y1 <= walker_y + new_walker_y <= y2:
+                if x1 <= walker_x + new_walker_x <= x2:
+                    if left == 1:
+                        up = 1
+                    elif right == 1:
+                        down = 1
+                    elif up == 1:
+                        left = 1
+                    elif down == 1:
+                        right = 1
+                elif y1 <= walker_y + new_walker_y <= y2:
+                    if left == 1:
+                        down = 1
+                    elif right == 1:
+                        up = 1
+                    elif up == 1:
+                        right = 1
+                    elif down == 1:
+                        left = 1
+
+                up *= velocity_reducer
+                down *= velocity_reducer
+                left *= velocity_reducer
+                right *= velocity_reducer
+
+                new_walker_x = (right - left) * velocity + (down - up) * velocity
+                new_walker_y = (left - right) * velocity + (down - up) * velocity
+                isometric_new_walker_x = 2 * (right - left) * velocity
+                isometric_new_walker_y = (down - up) * velocity
+
+                if  x1 <= walker_x + new_walker_x <= x2 and y1 <= walker_y + new_walker_y <= y2:
+
+                    walker_x += new_walker_x
+                    walker_y += new_walker_y
+                    isometric_walker_x += isometric_new_walker_x
+                    isometric_walker_y += isometric_new_walker_y
+                break     
+        
+    screen.fill((0, 0, 0)) 
+    screen.blit(image, image_rect)
+
+ 
+
+    # pygame.draw.circle(screen, (0, 255, 0), square_point, 5)
+    # pygame.draw.circle(screen, (0, 255, 0), isometric_point, 5)
+
+    square_point_to_draw = (walker_x, walker_y)
+
+    # [(0, -15), (-30, 0), (0, 15), (30, 0)]
+    isometric_position_vertices = []
+    for v in isometric_reference:
+        isometric_position_vertices.append((isometric_walker_x + v[0], isometric_walker_y + v[1]))
+    pygame.draw.polygon(screen, (0, 255, 0), isometric_position_vertices, 2)
+
+    square_position_vertices = [(walker_x - border, walker_y - border), (walker_x - border, walker_y + border), (walker_x + border, walker_y + border), (walker_x + border, walker_y - border)]
+    pygame.draw.polygon(screen, (0, 0, 255), square_position_vertices, 2)
+    pygame.draw.circle(screen, (0, 0, 0), square_point_to_draw, 5)
+
+    for v in isometrics:
+        pygame.draw.polygon(screen, (0, 255, 0), v, 2)  
+    pygame.draw.circle(screen, (0, 0, 0), (isometric_walker_x, isometric_walker_y), 5)
+    for v in squares_1:
+        pygame.draw.polygon(screen, (0, 0, 255), v, 2)   
+        # pygame.draw.polygon(screen, (0, 0, 0), v)
+    
+
+    pygame.display.flip()
+
+    clock.tick(60)
+
+
+
+pygame.quit()
+sys.exit()
---
+++ generate_square_from_isometric_refactor.py
@@ -0,0 +1,175 @@
+import pygame
+import sys
+import math
+
+pygame.init()
+
+SCREEN_WIDTH = 1200
+SCREEN_HEIGHT = 1000
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+clock = pygame.time.Clock()
+font = pygame.font.Font(None, 36)
+img_name = "simple_isometric"
+img_extension = "png"
+image = pygame.image.load(f"assets/{img_name}.{img_extension}")
+image_rect = image.get_rect()
+
+angle = math.radians(45)
+sin_angle = math.sin(angle)
+cos_angle = math.cos(angle)
+sqrt_2 = math.sqrt(2)
+
+x1 = 520
+x2 = 760
+y1 = 40
+y2 = 330
+
+square_type = "floor"
+final_types = []
+final_squares = []
+final_isometrics = []
+final_point_square = 0
+final_point_isometric = 0
+
+square_vertices = [(x1, y1), (x1, y2), (x2, y2), (x2, y1)]
+isometric_vertices = []
+for x, y in square_vertices:
+    new_x = 2*(x * cos_angle - y * sin_angle)/sqrt_2
+    new_y = (x * sin_angle + y * cos_angle)/sqrt_2
+    isometric_vertices.append((new_x, new_y))
+
+square_point = square_vertices[0]
+x = square_point[0]
+y = square_point[1]
+new_x = 2*(x * cos_angle - y * sin_angle)/sqrt_2
+new_y = (x * sin_angle + y * cos_angle)/sqrt_2
+isometric_point = (new_x, new_y)
+
+pressing_space = False
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_SPACE:
+                pressing_space = False
+                print("false")
+        
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                pressing_space = True
+                print("true")
+                continue
+            elif event.key == pygame.K_w:
+                y1 += 10 if pressing_space else -10
+            elif event.key == pygame.K_a:
+                x1 += 10 if pressing_space else -10
+            elif event.key == pygame.K_s:
+                y2 += -10 if pressing_space else 10
+            elif event.key == pygame.K_d:
+                x2 += -10 if pressing_space else 10
+            elif event.key == pygame.K_1:
+                square_type = "floor"
+            elif event.key == pygame.K_2:
+                square_type = "npc"
+            elif event.key == pygame.K_3:
+                square_type = "door"
+            elif event.key == pygame.K_UP:
+                square_point = (square_point[0], square_point[1] - 10)
+            elif event.key == pygame.K_DOWN:
+                square_point = (square_point[0], square_point[1] + 10)
+            elif event.key == pygame.K_LEFT:
+                square_point = (square_point[0] - 10, square_point[1])
+            elif event.key == pygame.K_RIGHT:
+                square_point = (square_point[0] + 10, square_point[1])
+            elif event.key == pygame.K_n:
+                final_squares.append(square_vertices)
+                final_isometrics.append(isometric_vertices)
+                continue
+            elif event.key == pygame.K_b:
+                final_squares.pop()
+                final_isometrics.pop()
+                continue
+            elif event.key == pygame.K_m:
+                final_point_square = square_point
+                final_point_isometric = isometric_point
+                continue
+            
+            square_vertices = [(x1, y1), (x1, y2), (x2, y2), (x2, y1)]
+            isometric_vertices = []
+            for x, y in square_vertices:
+                new_x = 2*(x * cos_angle - y * sin_angle)/sqrt_2
+                new_y = (x * sin_angle + y * cos_angle)/sqrt_2
+                isometric_vertices.append((round(new_x), round(new_y)))
+
+            print(isometric_vertices)
+            x = square_point[0]
+            y = square_point[1]
+            new_x = 2*(x * cos_angle - y * sin_angle)/sqrt_2
+            new_y = (x * sin_angle + y * cos_angle)/sqrt_2
+            isometric_point = (round(new_x), round(new_y))
+            print(square_vertices)
+
+
+    screen.fill((0, 0, 0)) 
+    screen.blit(image, image_rect)
+
+    for i, (x, y) in enumerate(isometric_vertices, start=1):
+        text_surface = font.render(str(i), True, (255, 255, 255))  
+        text_rect = text_surface.get_rect(center=(x, y - 15)) 
+        screen.blit(text_surface, text_rect)
+
+    for i, (x, y) in enumerate(square_vertices, start=1):
+        text_surface = font.render(str(i), True, (255, 255, 255))  
+        text_rect = text_surface.get_rect(center=(x, y - 15))  
+        screen.blit(text_surface, text_rect)
+
+
+    pygame.draw.polygon(screen, (255, 0, 0), isometric_vertices, 2)
+    pygame.draw.polygon(screen, (255, 0, 0), square_vertices, 2)
+    pygame.draw.circle(screen, (0, 255, 0), isometric_point, 5)  
+    pygame.draw.circle(screen, (0, 255, 0), square_point, 5)
+
+    for v in final_squares:
+        pygame.draw.polygon(screen, (0, 255, 0), v, 2)  
+    for v in final_isometrics:
+        pygame.draw.polygon(screen, (0, 255, 0), v, 2)  
+
+    
+    text_surface = font.render(square_type, True, (0, 0, 0))  
+    text_rect = text_surface.get_rect(center=(100, 100)) 
+    screen.blit(text_surface, text_rect)
+
+    pygame.display.flip()
+
+    clock.tick(60)
+
+
+# salvar aqui num txt
+
+file_path = f"assets/{img_name}.txt"
+
+# Open the file in write mode
+with open(file_path, "w") as file:
+
+    file.write("squares\n")
+    for item in final_squares:
+        file.write(str(item) + "\n")
+
+    file.write("\nisometrics\n")
+    for item in final_isometrics:
+        file.write(str(item) + "\n")
+
+    file.write("\nsquare_point\n")
+    file.write(str(final_point_square) + "\n")
+
+    file.write("\nisometric_point\n")
+    file.write(str(final_point_isometric) + "\n")
+
+
+pygame.quit()
+sys.exit()

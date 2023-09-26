4102e9bcb885d0a7d754646211182e6c037a63a9
moises-dias <moisesdepaulodias@gmail.com>
2023-09-25 22:40:26 -0300

    add movement between multiple squares

--- movement_refactor.py
+++ movement_refactor.py
@@ -1,24 +1,20 @@
 import pygame
 import sys
-import math
 
 pygame.init()
 
 SCREEN_WIDTH = 1200
 SCREEN_HEIGHT = 800
 
-squares = [[(520, 40), (520, 330), (760, 330), (760, 40)]]
-x1 = squares[0][0][0]
-y1 = squares[0][0][1]
-x2 = squares[0][2][0]
-y2 = squares[0][2][1]
+squares_1 = [
+    [(520, 40), (520, 330), (760, 330), (760, 40)],
+    [(760, 40), (760, 170), (880, 170), (880, 40)]
+]
 
-# isometrics = [[(480, 280), (190, 425), (430, 545), (720, 400)]]
+
 isometrics = [
     [(480, 280), (190, 425), (430, 545), (720, 400)],
-    [(480, 280), (310, 365), (400, 410), (570, 325)],
-    [(570, 325), (530, 345), (570, 365), (610, 345)],
-    [(300, 370), (210, 415), (260, 440), (350, 395)]
+    [(720, 400), (590, 465), (710, 525), (840, 460)]
 ]
 
 square_point = (660, 200)
@@ -50,10 +46,8 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    new_walker_x_h = 0
-    new_walker_y_h = 0
-    new_walker_x_v = 0
-    new_walker_y_v = 0
+    new_walker_x = 0
+    new_walker_y = 0
     isometric_new_walker_x = 0
     isometric_new_walker_y = 0
 
@@ -74,70 +68,89 @@ while running:
     if keys[pygame.K_DOWN]:
         down = 1
 
-    direction_sum = up + down + left + right
-    if (direction_sum) == 1:
-        if left == 1:
-            if not (x1 <= walker_x - velocity):
-                if (walker_y + velocity <= y2):
-                    down = 1   
-            elif not (walker_y + velocity <= y2):
-                if (x1 <= walker_x - velocity):
-                    up = 1
 
-        elif right == 1:
-            if not (walker_x + velocity <= x2):
-                if (y1 <= walker_y - velocity):
-                    up = 1   
-            elif not (y1 <= walker_y - velocity):
-                if (walker_x + velocity <= x2):
-                    down = 1
+    # se o resultado ta dentro de algum squares_1, seta os moves e vai pro proximo
+    updated_1 = False
+    for sq1 in squares_1:
+        x1 = sq1[0][0]
+        y1 = sq1[0][1]
+        x2 = sq1[2][0]
+        y2 = sq1[2][1]
 
-        elif up == 1:
-            if not (x1 <= walker_x - velocity):
-                if (y1 <= walker_y - velocity):
-                    right = 1   
-            elif not (y1 <= walker_y - velocity):
-                if (x1 <= walker_x - velocity):
-                    left = 1
-
-        elif down == 1:
-            if not (walker_y + velocity <= y2):
-                if (walker_x + velocity <= x2):
-                    right = 1   
-            elif not (walker_x + velocity <= x2):
-                if (walker_y + velocity <= y2):
-                    left = 1
-
-    if direction_sum != (up + down + left + right):
-        up *= velocity_reducer
-        down *= velocity_reducer
-        left *= velocity_reducer
-        right *= velocity_reducer
-        
-
-    if (right - left) != 0:
-        new_walker_x_h = (right - left) * velocity
-        new_walker_y_h = (left - right) * velocity
+        new_walker_x = (right - left) * velocity + (down - up) * velocity
+        new_walker_y = (left - right) * velocity + (down - up) * velocity
         isometric_new_walker_x = 2 * (right - left) * velocity
-    if (up - down) != 0:
-        new_walker_x_v = (down - up) * velocity
-        new_walker_y_v = (down - up) * velocity
         isometric_new_walker_y = (down - up) * velocity
 
-    if  x1 <= walker_x + new_walker_x_h + new_walker_x_v <= x2 and y1 <= walker_y + new_walker_y_h + new_walker_y_v <= y2:
-        walker_x += new_walker_x_h
-        walker_y += new_walker_y_h
-        isometric_walker_x += isometric_new_walker_x
+        if  x1 <= walker_x + new_walker_x <= x2 and y1 <= walker_y + new_walker_y <= y2:
 
-        walker_x += new_walker_x_v
-        walker_y += new_walker_y_v
-        isometric_walker_y += isometric_new_walker_y
+            walker_x += new_walker_x
+            walker_y += new_walker_y
+            isometric_walker_x += isometric_new_walker_x
+            isometric_walker_y += isometric_new_walker_y
+
+            updated_1 = True
+            break
+
+    # se nao atualizou é pq tentou sair dos limites
+    # preciso checar se so uma direcao foi pressionada???????????
+    if not updated_1 and (up + down + left + right) == 1:
+        # se chegou ate aqui é uma ou as duas direcoes nao fica dentro de um quadrado andavel
+        # se x nao esta em nenhum quadrado, e y nao esta em nenhum quadrado, nao anda
+        # senao
+        # se x em algum quadrado, anda em x
+        # se y em algum quadrado, anda em y
+
+        for sq1 in squares_1:
+            x1 = sq1[0][0]
+            y1 = sq1[0][1]
+            x2 = sq1[2][0]
+            y2 = sq1[2][1]
+
+            if  not (x1 <= walker_x <= x2 and y1 <= walker_y <= y2):
+                continue
+
+            if  x1 <= walker_x + new_walker_x <= x2 or y1 <= walker_y + new_walker_y <= y2:
+                print("X ou Y dentro")
+                if x1 <= walker_x + new_walker_x <= x2:
+                    if left == 1:
+                        up = 1
+                    elif right == 1:
+                        down = 1
+                    elif up == 1:
+                        left = 1
+                    elif down == 1:
+                        right = 1
+                elif y1 <= walker_y + new_walker_y <= y2:
+                    if left == 1:
+                        down = 1
+                    elif right == 1:
+                        up = 1
+                    elif up == 1:
+                        right = 1
+                    elif down == 1:
+                        left = 1
+
+                up *= velocity_reducer
+                down *= velocity_reducer
+                left *= velocity_reducer
+                right *= velocity_reducer
+
+                new_walker_x = (right - left) * velocity + (down - up) * velocity
+                new_walker_y = (left - right) * velocity + (down - up) * velocity
+                isometric_new_walker_x = 2 * (right - left) * velocity
+                isometric_new_walker_y = (down - up) * velocity
+
+                walker_x += new_walker_x
+                walker_y += new_walker_y
+                isometric_walker_x += isometric_new_walker_x
+                isometric_walker_y += isometric_new_walker_y
+                break     
         
     screen.fill((0, 0, 0)) 
     screen.blit(image, image_rect)
 
  
-    square_to_draw = [(x, y) for x,y in squares[0]]
     square_point_to_draw = (walker_x, walker_y)
 
     pygame.draw.circle(screen, (0, 255, 0), square_point, 5)
@@ -147,11 +160,10 @@ while running:
 
     for v in isometrics:
         pygame.draw.polygon(screen, (0, 255, 0), v, 2)  
-    for v in squares:
-        pygame.draw.polygon(screen, (0, 255, 0), v, 2)  
+    for v in squares_1:
+        pygame.draw.polygon(screen, (0, 255, 0), v, 2)   
+        # pygame.draw.polygon(screen, (0, 0, 0), v)
     
-    pygame.draw.polygon(screen, (0, 255, 0), square_to_draw, 2)  
-
 
     pygame.display.flip()
 
